38d5e05a7045b55102fa1789f659f3ae12103f05
joshkaye88 <jkaye@shaw.ca>
2022-11-01 22:06:56 -0700

    removed old handleSubmit on AddBookForm.js

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,5 +1,4 @@
 
-import { getGoogleBookInfo } from "../utils/google"
 import { useRef } from "react"
 
 export default function AddBookForm ( { LIBRARYMODEL, library, user, setLibrary, setUserAction, setFlash, sortMethod, filterMethod } ) {
@@ -7,82 +6,6 @@ export default function AddBookForm ( { LIBRARYMODEL, library, user, setLibrary,
   const wrapper = useRef()
 
     
-
-  async function oldhandleSubmit(e) {
-    
-    e.preventDefault()
-    
-
-    // Get Form Data and Set Variables
-    let isbn = e.target[0].value
-    let bookIsRead = e.target[1].checked
-    
-    
-    // Check if ISBN is entered
-    if (!isbn) {
-      setFlash({
-        message : 'Please Enter an ISBN',
-        type : 'fail'
-      })
-      return
-    }
-    
-    // Check if book is already in the users library
-    const userHasBook = library.find( book => book.isbn === isbn )
-    
-    if(userHasBook) {
-      setFlash({
-        message : `This book is already in your Library.`,
-        type : 'fail',
-        link : `#${isbn}`,
-        linkText : 'View Book >'
-      })
-      return
-    }
-  
-    // Get book data from google
-    let res
-    try {
-      res = await getGoogleBookInfo(isbn)
-    } catch (error) {
-      console.log(error)
-      return 'Error fetching book data from Google API'
-    }
-
-    // Check if book has cover (some dont)
-    let cover = (res.imageLinks !== undefined) ? res.imageLinks.thumbnail : false
-
-
-    // Add book entry to DB
-    const newBook = {
-      isbn,
-      bookIsRead,
-      cover,
-      date : new Date().valueOf(),
-      title : res.title,
-      author : res.authors[0],
-      pages : res.pageCount,
-      userId : user.id,
-    }
-
-    const response = await LIBRARYMODEL.addBook(newBook)
-
-    // Update Library in UI
-    const updatedLibrary = await LIBRARYMODEL.getLibrary(user.id, sortMethod, filterMethod)
-    setLibrary(updatedLibrary)
-
-    // Close Add Book Form
-    setUserAction(false)
-
-    // Update flash message
-    setFlash({
-      message : `${ res.title } Added Successfully`,
-      type : 'success',
-      link : `#${ isbn }`,
-      linkText : 'View Book'
-    })
-
-  }
 
   async function handleSubmit(e) {
     e.preventDefault()
